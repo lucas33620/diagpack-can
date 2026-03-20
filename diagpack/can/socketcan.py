463cf159869c134ccd112007a7d51556d06224d7
VMLucas <l.baquey@syloria.fr>
2026-03-20 18:53:30 +0100

    DP-004: add CAN frame reader

--- diagpack/can/socketcan.py
+++ diagpack/can/socketcan.py
@@ -7,7 +7,10 @@
 """
 
 import socket
+import struct
 
+CAN_FRAME_FORMAT = "=IB3x8s" # 4 + 1 + 3 + 8 = 16 octets
+CAN_FRAME_SIZE = struct.calcsize(CAN_FRAME_FORMAT)
 
 def open_can_socket(interface: str) -> socket.socket:
     try:
@@ -19,3 +22,20 @@ def open_can_socket(interface: str) -> socket.socket:
         ) from exc
 
     return can_socket
+
+def read_frame(can_socket: socket.socket) -> tuple [int, int, bytes]:
+    if can_socket is None :
+        raise RuntimeError(
+            f"Socket CAN interface is not initialized"
+        )
+
+    frame = can_socket.recv(CAN_FRAME_SIZE)
+
+    if CAN_FRAME_SIZE != len(frame):
+        raise RuntimeError(
+            f"Incomplete CAN frame received: expected {CAN_FRAME_SIZE} bytes, got {len(frame)}"
+        )
+    
+    can_id, dlc, data = struct.unpack(CAN_FRAME_FORMAT, frame)
+
+    return can_id, dlc, data[:dlc]
